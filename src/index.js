4656485d0228718eb27195bf8a68255cc5e57e84
katherinefedenko <katyafedenko29@gmail.com>
2019-03-20 12:11:39 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var pop = {};
+  var stack = [];
+  for(var open = 0; open<bracketsConfig.length; open++){
+        pop[bracketsConfig[open][1]] = bracketsConfig[open][0];
 }
+    for(var i=0;i<str.length;i++){
+        var char = str.charAt(i);
+        if(!pop[char] && char !=='|' && typeof char !== 'number'){
+            stack.push(char);
+        }
+        else if(pop[char]==char && !stack.includes(char)){
+            stack.push(char);
+        }
+        else if(pop[char]==char && stack.includes(char)){
+            if(stack.pop()!=pop[char]) return false;
+        }
+        else{
+            if(stack.length==0 || stack.pop()!=pop[char])return false;
+        }
+    }
+    return stack.length==0?true:false;
+}
+
